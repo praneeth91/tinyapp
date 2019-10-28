e18d0cb8551ad367c6d65f9a1ef08e2f3f53d8b8
Praneeth <praneethabc123@gmail.com>
2019-10-28 22:03:38 +0000

    Updated

--- express_server.js
+++ express_server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,4 +13,12 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
