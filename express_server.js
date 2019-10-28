1eee362aa4098ec886c2e27f3d0e9d8e358abf90
Praneeth <praneethabc123@gmail.com>
2019-10-28 22:48:45 +0000

    Updating the views

--- express_server.js
+++ express_server.js
@@ -11,6 +11,10 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
